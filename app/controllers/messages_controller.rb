11b31718ee7cdbcad354daeff13111b8bcde1bfa
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-11 15:54:11 +0900

    redirect to sign_in
    メッセージコントローラーのbefore_actionを、ログインしていない場合にsign_inページにリダイレクトするように設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
 
-  before_action :move_to_signup, except: :index
+  before_action :move_to_signup
 
   def index
   end
 
   private
   def move_to_signup
-    redirect_to "/users/sign_up" unless user_signed_in?
+    redirect_to "/users/sign_in" unless user_signed_in?
   end
 end
