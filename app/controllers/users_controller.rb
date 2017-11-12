29cfb50d62059cd9f82b0ae07abae6b7f022a768
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-12 13:36:33 +0900

    ADD users update function
    users_controllerにform_forからのupdateアクションの機能を追加。
    サインインしていないとサインイン画面にリダイレクトされる機能も追加。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,26 @@
 class UsersController < ApplicationController
 
+  before_action :move_to_signin, only: :edit
+
   def edit
   end
 
   def update
-
-    redirect_to "/"
+    if current_user.update(user_params)
+      redirect_to "/"
+    else
+      render :edit
+    end
   end
 
+# requireをつける
   private
   def user_params
-    params.permit(:name, :email)
+    params.require(:user).permit(:name, :email)
+  end
+
+  def move_to_signin
+    redirect_to "/users/sign_in" unless user_signed_in?
   end
 
 end
