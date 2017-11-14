8aa2525fa9aabb9fe0c5e94b81d1bb2273b541a4
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-14 15:02:54 +0900

    グループの作成、編集機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,34 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] = "グループを作成しました"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:notice] = "グループを編集しました"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
