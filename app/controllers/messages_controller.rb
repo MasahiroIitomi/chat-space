5d9b0fc15028529f96c488a5c5db05d2e9b0591e
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-16 02:35:09 +0900

    Add before_action set_group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
+
   def index
-    @group = Group.find(params[:group_id])
     @message = Message.new
     @messages = Message.where(group_id: params[:group_id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path(params[:group_id])
@@ -19,5 +19,9 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
