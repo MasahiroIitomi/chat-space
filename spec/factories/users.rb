470dc589e647f81adafadc9bd6a952d70554fb80
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-17 20:31:21 +0900

    passwordの文字列のFaker化

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     name        Faker::Name.name
     email       Faker::Internet.email
-    password    "testpass"
+    password    Faker::Lorem.characters(10)
   end
 end
