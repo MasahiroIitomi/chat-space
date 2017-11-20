b14541c3c881162a12efb8f4cd3957a77f292e81
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-20 20:58:19 +0900

    contextでログイン、非ログイン時を分岐

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -7,31 +7,26 @@ describe MessagesController do
      {group_id: group.id}
   end
   describe 'GET #index' do
-    describe 'login' do
+    context 'login' do
       before do
         login_user user
         get :index, group_id: group
       end
-
       it "assigns the requested group to @group" do
         expect(assigns(:group)).to eq group
       end
-
       it "assigns the requested message to @message" do
         expect(assigns(:message)).to be_a_new(Message)
       end
-
       it "assigns the requested messages to @messages" do
         messages = create_list(:message, 3)
         expect(assigns(:messages)).to match(messages)
       end
-
       it "renders the :index template" do
         expect(response).to render_template :index
       end
     end
-
-    describe 'logout' do
+    context 'logout' do
       it "redirect to the new_user_session_path" do
         get :index, group_id: group
         expect(response).to redirect_to new_user_session_path
@@ -74,5 +69,4 @@ describe MessagesController do
       end
     end
   end
-
 end
