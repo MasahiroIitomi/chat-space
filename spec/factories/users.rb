fb4afb97275cbed4ffcab97ec2edf22c782e61ef
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-20 20:16:28 +0900

    factories 修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name        Faker::Name.name
     email       Faker::Internet.email
     password    Faker::Lorem.characters(10)
+    initialize_with { User.find_or_create_by(email: email)}
   end
 end
