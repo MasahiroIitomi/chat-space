92392b7a88f8ddf98e8e485c95968d45c078841e
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-22 18:51:53 +0900

    jsファイルの動作定義

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,25 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${ message.user_name }
+                    </div>
+                    <div class="upper-message__date">
+                      ${ message.created_at }
+                    </div>
+                  </div>
+                  <div class="lower-message__content">
+                    ${ message.body }
+                  </div>
+                  <div class="lower-message__content--image">
+                    <img src="${ message.image }">
+                  </div>
+                </div>`
+    return html;
+  }
   $('#comment_form').on('submit', function(e){
     e.preventDefault();
-    console.log(this);
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -12,5 +30,18 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.text-input').val('');
+      $(".send-btn").prop("disabled", false);
+      $(".messages").animate({
+        scrollTop: $(".messages")[0].scrollHeight
+      });
+    })
+    .fail(function(){
+      $(".send-btn").prop("disabled", false);
+      alert('error');
+    });
   });
 });
