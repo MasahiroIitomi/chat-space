d3001fcfa2aad96b5ef2ada90baa759aa1cb1dca
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-11-25 18:16:42 +0900

    メッセージの一覧画面におけるjsonへ送るためのコントローラーのアクションを指定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = Message.where(group_id: params[:group_id])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
